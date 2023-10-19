6ce0ea73b43bb776e02af0ad6dfdac974e462477
Jiebin Su <jiebin.su@krakenflex.com>
2023-10-20 00:42:20 +0100

    chore: setup jest and add first test

---
+++ src/app/page.test.tsx
@@ -0,0 +1,10 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from '@testing-library/react'
+import Page from './page'
+
+it('Renders page successfully', () => {
+  render(<Page />)
+  expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Docs')
+})
